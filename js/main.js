90ce4d9b38c7e9378cb333619b538a0dc0b43223
Afonso-cmd <afonso.ur@gmail.com>
2021-05-05 16:47:43 -0300

    Variáveis e console

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var nome = "Desenvolvedor Afonso Henriques";
+var idade = 27;
+var idade2 = 3;
+var frase = "Vai Corinthians"
+//alert("Olá " + nome+" idade: "+idade+" anos");
+//alert(idade+idade2); 
+
+//Para exibição no console do navegador
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase);
+console.log(frase.replace("Corinthians","Brasil"));//troca de elementos do String
+console.log(frase.toUpperCase());//Maiúsculas
+console.log(frase.toLowerCase());//Minúsculas 
+
+//Operações
+
+var n1 = 5;
+var n2 = 2;
+
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
