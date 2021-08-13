eddad18785389d8ec96404915d36656afd962bcc
Afonso-cmd <afonso.ur@gmail.com>
2021-08-13 12:14:58 -0300

    function arrowfn

--- js/main.js
+++ js/main.js
@@ -1,44 +1,13 @@
-var lista = ["Afonso", "Henriques"];
-console.log (lista);//imprime o array inteiro
+function fn (){// declaração de função comum
+	return 'teste1';
+}
 
-console.log(lista[1]); //imprime a posição 1 do array
-alert(lista[0]);//Idem linha superior com Alert na posição 0 do array
+const arrowFn0 = () => 'teste2'; //arrow function, não preciso abrir o bloco para uma declaração
+const arrowFn1 = () => {
+	//Para mais de uma expressão
+	return 'teste3';
+}
 
-//adicioando elementos na lista
-lista.push("Silva");
-console.log (lista);
-
-//tirar ultimo elemento da lista
-lista.pop();
-console.log (lista);
-
-//exibindo o quantidade de posições no array
-console.log(lista.length);
-
-//inverter a exibição dos elementos
-//console.log(lista.reverse());
-
-//para exibição em formato String
-console.log(lista.toString());
-
-//para exibição da primeira letra do primeiro elemento
-console.log(lista.toString()[0]);
-
-//para exibição dos elementos sem a vírgula
-console.log(lista.join(" "));//com espaço no caso, poderia ser com -: join("-")
-
-
-//********DICIONÁRIO*********
-var nome = {primeiro:"Afonso", segundo:"Henriques"}
-console.log(nome);
-
-//elemento especifico 
-console.log(nome.primeiro);
-
-
-//******LISTA DE DICIONÁRIOS*******
-var nomes = [{primeiro:"Afonso", segundo:"Henriques"}, {primeiro:"Felipe", segundo:"Lagedo"}]
-console.log(nomes);
-
-//alert na posição especifica 
-alert(nomes[1].segundo);
+document.getElementById("demostracao0").innerHTML = fn();//exibição no html no id demostracao
+document.getElementById("demostracao1").innerHTML = arrowFn0();
+document.getElementById("demostracao2").innerHTML = arrowFn1();
