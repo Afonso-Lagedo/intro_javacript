4f7ed54fd7194b02d3779fc068946a90a7ec000e
Afonso-cmd <afonso.ur@gmail.com>
2021-08-15 18:31:05 -0300

    for in/of exemplo2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-let arr = [3,5,7];
-arr.foo = "olá";
+let arr = [10,20,30,40];
+arr.foo = "olá";//proriedade foo
 // i == array position
 
 for (let i in arr){//para cada elemento me traga a posição
 	console.log(i);
 }
 
-for (let i of arr){//para cada elemento me traga o valor
+for (let i of arr){//para cada elemento me traga o valor !Apenas valores numerados! por isso que não vem o "olá"
 	console.log(i);
 }
