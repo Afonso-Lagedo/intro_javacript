48b6b35093639dd7d68f9e0d758f3bb0134c342c
Afonso-cmd <afonso.ur@gmail.com>
2021-08-15 17:57:35 -0300

    while

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const array = ['camaro', 'corsa', 'uno'];
+var n = 0;
 
-for (let position = 0; position < array.length; position++){//percorrendo o vetor
-	const element = array[position];//recupera a posição do vetor
-	console.log(`Elemento #${position}: ${element}.`);//repuperando posição e valor	
+while (n<4){//enquanto isso for verdade...
+	//..faça:
+	n++;
+	console.log(n);
 }
-
