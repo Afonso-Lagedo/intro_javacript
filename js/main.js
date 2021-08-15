0c0af9529e9c890a4dfb80946c0261652f372e62
Afonso-cmd <afonso.ur@gmail.com>
2021-08-15 19:03:35 -0300

    my first class

--- js/main.js
+++ js/main.js
@@ -1,28 +1,9 @@
-//break
+'use strict';
 
-var valor = 0;//declaração
-
-while(true){//enquanto for verdade
-	valor++;//incrementa
-
-	if (valor>3){//se isso for verdade...
-		break;//...para o programa
-	}
-
-	console.log(valor);//exibindo
+function Carro(){
+	this.qntRoda = 4;
 }
 
+const camaro = new Carro();
 
-//continue
-
-const array = [1,2,3,4,5,6,7];
-
-for (let posicao = 0; posicao < array.length; posicao++){
-	const valor = array[posicao];
-
-	if (valor %2 === 0){
-		continue;//pula quando no caso a condição for verdadeira
-	}
-
-	console.log(valor);
-}
+console.log(camaro.qntRoda);
