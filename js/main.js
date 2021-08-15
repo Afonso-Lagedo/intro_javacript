64de1b45b71e561098d019aff59669ddb89c4611
Afonso-cmd <afonso.ur@gmail.com>
2021-08-15 20:51:37 -0300

    creating class properly

--- js/main.js
+++ js/main.js
@@ -1,37 +1,17 @@
-function Pessoa (nome){
-	this.nome = nome;
+'use strict'
+
+class Carro {
+	constructor(qntRodas){
+		this.qntRodas=4;
+	}
 }
 
-const p = new Pessoa('Afonso');
-
-console.log(p);
-
-function Person (name){
-	this.name = name;
-
-	return{// return explícito
-		name:'Teste'
-	};
-}
-
-const p2 = new Person('Afonso');
-
-console.log(p2);	
-
-//teste de tipo
-console.log(p instanceof Pessoa);//true 
-
-console.log("#############");
-
-function Carro (qntRodas){
-	this.qntRodas = qntRodas;
-}
-
-function MuscleCar(rapido){
-	Carro.call(this, 4);//contextualizando a roda para o camaro
-
-	this.rapido=rapido;
-}
+class MuscleCar extends Carro{
+	constructor(rapido){
+		super(4);
+		this.rapido = "sim";
+	}
+} 
 
 const camaro = new MuscleCar(true);
 
