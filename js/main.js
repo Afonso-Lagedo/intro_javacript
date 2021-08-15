25b1decba01ff44faf192dfa3f3afc1f91735fa9
Afonso-cmd <afonso.ur@gmail.com>
2021-08-15 20:35:26 -0300

    creating object

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-'use strict';
-
-function Carro(){
-	this.qntRoda = 4;
+function Pessoa (nome){
+	this.nome = nome;
 }
 
-const camaro = new Carro();
+const p = new Pessoa('Afonso');
 
-console.log(camaro.qntRoda);
+console.log(p);
+
+function Person (name){
+	this.name = name;
+
+	return{// return explícito
+		name:'Teste'
+	};
+}
+
+const p2 = new Person('Afonso');
+
+console.log(p2);	
+
+//teste de tipo
+console.log(p instanceof Pessoa);//true 
+
+console.log("#############");
+
+function Carro (qntRodas){
+	this.qntRodas = qntRodas;
+}
+
+function MuscleCar(rapido){
+	Carro.call(this, 4);//contextualizando a roda para o camaro
+
+	this.rapido=rapido;
+}
+
+const camaro = new MuscleCar(true);
+
+console.log(camaro);
